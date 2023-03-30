2bdc2160aa6b4c2d4341df6af5c1849ae3335971
lazerface175 <124861983+lazerface175@users.noreply.github.com>
2023-03-30 00:11:53 -0600

    added function to set how many divs to add

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const mainBox = document.querySelector("#mainBox")
+const setSize = document.querySelector("#setSize")
+let gridSize = document.querySelector("#setSize").addEventListener("click", setGrid)
 
-for ( i = 0; i < 10; i++) {
+function setGrid() {
+    gridSize = prompt("Enter grid size");
+    while (mainBox.hasChildNodes ()) {mainBox.removeChild (mainBox.lastChild);};
+    addGrid();
+}
+
+function addGrid () {
+    for ( i = 0; i < gridSize; i++) {
     //document.createElement("div").classList.add("item");
     //document.body.appendChild(document.createElement("div"));
     //document.getElementById("mainBox").appendChild(document.createElement("div").classList.add("test"));
     const container = document.createElement("div");
     container.classList.add("pixels");
     mainBox.appendChild(container);
-}
+}}
