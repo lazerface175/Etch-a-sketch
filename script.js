6f86ff84c4ac8d1cd5a658d8665e6ebfa00309ad
lazerface175 <124861983+lazerface175@users.noreply.github.com>
2023-04-07 22:28:23 -0700

    added pixel adjusting for grid size functionality

--- script.js
+++ script.js
@@ -4,16 +4,33 @@ let gridSize = document.querySelector("#setSize").addEventListener("click", setG
 
 function setGrid() {
     gridSize = prompt("Enter grid size");
-    while (mainBox.hasChildNodes ()) {mainBox.removeChild (mainBox.lastChild);};
-    addGrid();
+    amountOfSquares = Math.pow(gridSize, 2);
+    while (mainBox.hasChildNodes () == true) {mainBox.removeChild (mainBox.lastChild);};
+
+    //let containerSize = document.getElementById("pixels").style.width=`${100+100}px`;
+
+    if (gridSize > 100) {
+        alert("Grid size cannot be greater than 100");
+    } else {
+        addGrid();
+    }
+
 }
 
+
+
 function addGrid () {
-    for ( i = 0; i < gridSize; i++) {
+    for ( i = 0; i < amountOfSquares; i++) {
     //document.createElement("div").classList.add("item");
     //document.body.appendChild(document.createElement("div"));
     //document.getElementById("mainBox").appendChild(document.createElement("div").classList.add("test"));
-    const container = document.createElement("div");
-    container.classList.add("pixels");
-    mainBox.appendChild(container);
-}}
+    // container.style.width = `${gridSize}px`;
+    // container.style.height = `${gridSize}px`;
+    const pixels = document.createElement("div");
+    const canvasArea = 250000;
+    pixels.classList.add("pixels");
+    pixels.style.width = `${Math.sqrt(canvasArea/amountOfSquares)}px`;
+    mainBox.appendChild(pixels);
+};
+
+}
